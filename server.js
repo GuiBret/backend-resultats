50f1cf760a31ef542c4fe530dfc70bcd0efb7551
GuiBret <guillaume.bretzner@gmail.com>
2018-01-15 20:27:45 +0100

    ajout tests

--- server.js
+++ server.js
@@ -9,40 +9,12 @@ var express = require("express"),
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("X-Auth-Token", process.env.FOOTBALLAPIKEY);
   next();
 });
 
 
 
 let cm = new ConnectionManager(app);
-/*
-app.get("/", (req, res) => {
 
-    var get_req = http.get("http://api.football-data.org/v1/fixtures?timeFrame=n1", (data_res) => {
-        
-        let rawData = '';
-        data_res.on('data', (chunk) => { rawData += chunk; });
-        
-        data_res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-            res.header('Access-Control-Allow-Origin', "*");
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type');
-            
-            res.send(parsedData);
-        } catch (e) {
-          console.error(e.message);
-        
-        }
-      
-        });
-        
-    });
-    
-    
-});
-*/
 
 app.listen(3000);
--- spec/connectionmanager.spec.js
+++ spec/connectionmanager.spec.js
@@ -23,31 +23,6 @@ describe("app routing", function() {
 });
 */
 
-describe("cm.getCountryList", function() {
-    
-    beforeAll(function() {
-       this.cm = new ConnectionManager(app);
-        this.res = { // Fake res object
-            send: function() {
-                
-            }
-        }
-    });
-    
-    it("should check if the utils.getCountryList function has been called", function() {
-        spyOn(this.cm.utils, "getCountryList");
-        this.cm.getCountryList({}, this.res);
-        
-        expect(this.cm.utils.getCountryList).toHaveBeenCalled();
-        
-    });
-    it("should return the list of all available countries and their ID", function() {
-        spyOn(this.res, "send");
-        this.cm.getCountryList({}, this.res);
-        
-        expect(this.res.send).toHaveBeenCalledWith(jasmine.any(Object));
-    })
-});
 
 describe("cm.getCountryList", function() {
     
@@ -74,8 +49,62 @@ describe("cm.getCountryList", function() {
         expect(this.res.send).toHaveBeenCalledWith(jasmine.any(Object));
     });
 });
+
 
 describe("cm.getCompetitionResults", function() {
+   beforeAll(function() {
+       this.cm = new ConnectionManager(app);
+        this.res = { // Fake res object
+            send: function() {
+                
+            }
+        };
+       this.req = {
+           params: {
+               idcompetition: 426 // Premier League 2016
+           }
+       }
+   });
+    
+    it("should have returned proper data", function(done) {
+        spyOn(this.res, "send");
+       this.cm.getCompetitionResults(this.req, this.res).then((data) => {
+           
+            expect(this.res.send).toHaveBeenCalledWith(jasmine.any(Object));  
+            done();
+       });
+        
+    });
+});
+
+
+
+describe("cm.getCompetitionRankings", function() {
+    
+    beforeAll(function() {
+        this.cm = new ConnectionManager(app);
+        this.res = { // Fake res object
+            send: function() {
+                
+            }
+        }
+        
+        this.req = {
+            idcompetition: 432
+        }
+    })
+   it("should not return anything if it is not a championship", function(done) {
+       this.cm.getCompetitionRankings(this.req, this.res).then((data) => {
+           console.log("Data : ");
+            console.log(data);
+           done();
+       });
+   });
+});
+
+
+
+describe("cm.getData", function() {
    beforeAll(function() {
        this.cm = new ConnectionManager(app);
         this.res = { // Fake res object
@@ -88,11 +117,37 @@ describe("cm.getCompetitionResults", function() {
                idcompetition: 398 // Premier League
            }
        }
+       
    });
     
-    it("should check if getCompetitionResults has return the proper data", function(done) {
+    it("should catch an error if the data is restricted (prior to 2016)", function(done) {
         
-       this.cm.getCompetitionResults(this.req, this.res);
-        done();
+        this.cm.getData("http://api.football-data.org/v1/competitions/398").then((data) => {
+            //console.log('Attente restricted');
+            //expect(err.error).toEqual("The resource you are looking for is restricted");
+            done();
+        }).catch((err) => {
+            console.log('Attente restricted');
+            expect(err.error).toEqual("The resource you are looking for is restricted");
+            done();
+    
+        });
     });
+    
+    it("should return proper data if we select the results of a specific competition", function(done) {
+        this.cm.getData("http://api.football-data.org/v1/competitions/426/fixtures/").then((data) => { // PremierLeague 2016
+    
+            expect(data.count).toEqual(380); // Should contain 380 games
+            done();
+        })
+    });
+    
+    it("should return proper data if we select the ranking of a selected competition", function(done) {
+        this.cm.getData("http://api.football-data.org/v1/competitions/426/leagueTable").then((data) => {
+           expect(data.matchday).toEqual(38); // The championship should be finished 
+            done();
+            
+        });
+    });
+    
 });
